de022688feb6aee4f8ef940eb076023aeb2574af
Michael Leonhard <77648651+MSLeonhard@users.noreply.github.com>
2021-02-03 15:57:19 -0500

    completed activity 3 app

---
+++ js/conditionals-3.js
@@ -0,0 +1,13 @@
+
+
+
+let userData = prompt("Give me an input!");
+
+//userData = parseInt(userData);
+
+if ( isNaN(userData) ) {
+    alert('This is not a number, try again.')
+} else {
+    alert('You input a number! Yay!')
+}
+    
