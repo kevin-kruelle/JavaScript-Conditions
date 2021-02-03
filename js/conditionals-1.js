cec3b343f64043af072820f6d796f1b38516a1b4
Michael Leonhard <77648651+MSLeonhard@users.noreply.github.com>
2021-02-03 14:53:17 -0500

    created random number guessing app

--- js/conditionals-1.js
+++ js/conditionals-1.js
@@ -4,3 +4,17 @@ console.log('conected...');
 1.Generate Number
 2. prompt user to type Number
 3. compare results (if/else) */
+
+const generatedNumber = Math.floor(Math.random() * 10) + 1;
+
+let userGuess = prompt("Guess what number I'm thinking of from 1 to 10!")
+
+userGuess = parseInt(userGuess)
+
+console.log(generatedNumber)
+
+if (userGuess === generatedNumber) {
+    document.querySelector('h1').innerHTML = 'You guessed right, great job!'
+} else {
+    document.querySelector('h1').innerHTML = 'You guessed wrong, refresh to try again.'
+}
